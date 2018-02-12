985e26c726df0b0e948f82f117a8ce12e751f262
Florestan De Moor <florestan.demoor@gmail.com>
2018-02-12 12:16:32 -0500

    Drop table problem should be really fixed now

--- gdk/gdk_bbp.c
+++ gdk/gdk_bbp.c
@@ -1374,6 +1374,13 @@ BBPreadEntries(FILE *fp, unsigned bbpversion)
 
 		BBP_desc(bid) = bn;
 		BBP_status(bid) = BBPEXISTING;	/* do we need other status bits? */
+
+		/* Set again pybat mask if it was set before */
+		if (status & BBPPYTHONBAT) {
+			BBP_status_on(bid, BBPPYTHONBAT, "BBPreadEntries");
+		}
+		/* */
+
 		if ((s = strchr(headname, '~')) != NULL && s == headname) {
 			snprintf(logical, sizeof(logical), "tmp_%o", (unsigned) bid);
 		} else {
@@ -1792,7 +1799,7 @@ new_bbpentry(FILE *fp, bat i, const char *prefix)
 		    BUNFMT " " OIDFMT, prefix,
 		    /* BAT info */
 		    (ssize_t) i,
-		    BBP_status(i) & BBPPERSISTENT,
+		    BBP_status(i) & (BBPPERSISTENT | BBPPYTHONBAT), /* Python BAT flag must be saved */
 		    BBP_logical(i),
 		    BBP_physical(i),
 		    BBP_desc(i)->batRestricted << 1,
@@ -3978,7 +3985,6 @@ BBPcacheBAT(BAT *b) {
 	}
 	mode = BBP_status(b->batCacheid) | BBPPYTHONBAT;
 	BBP_status_set(b->batCacheid, mode, "BBPcacheBAT");
-
 	BATassertProps(b);
 	return 1;
 }
